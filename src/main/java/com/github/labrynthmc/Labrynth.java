4c629e36a038a0123bbacdbb665ce706c52b3d96
Evan Severson <severson.evan@gmail.com>
2020-04-17 18:36:38 -0700

    Setup repository for labrynth mod

---
+++ src/main/java/com/github/labrynthmc/Labrynth.java
@@ -0,0 +1,12 @@
+package com.github.labrynthmc;
+
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.Arrays;
+
+@Mod(Labrynth.MODID)
+public class Labrynth {
+
+    public static final String MODID = "labrynthmc";
+
+}
