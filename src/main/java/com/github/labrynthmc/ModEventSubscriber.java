c73154657e97c0d751b5005faf5569f7c4b337bf
Brandon Henke <brandon.henke3@gmail.com>
2020-04-20 02:50:56 -0500

    Add structure templates, world load event detection, and generate maze on loading nether.

--- src/main/java/com/github/labrynthmc/ModEventSubscriber.java
+++ src/main/java/com/github/labrynthmc/ModEventSubscriber.java
@@ -2,9 +2,15 @@ package com.github.labrynthmc;
 
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.chunk.AbstractChunkProvider;
+import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.storage.WorldInfo;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistryEntry;
@@ -13,11 +19,28 @@ import net.minecraftforge.registries.IForgeRegistryEntry;
 public class ModEventSubscriber
 {
 	@SubscribeEvent
-	public void pickupItem(EntityItemPickupEvent event)
+	public void netherLabrynthWorldGen(WorldEvent.Load event)
 	{
-		Grid maze = Grid.genMaze(5,500);
-		System.out.println("Maze generated, at "+maze.getCenter()+", for no reason.");
+		if (!event.getWorld().isRemote()) {
+			IWorld world = event.getWorld();
+			Dimension dim = world.getDimension();
+			DimensionType dimType = dim.getType();
+
+			System.out.println("Dimension ID = " + dimType.getId());
+
+			switch (dimType.getId()) {
+				case -1:
+					Grid maze = Grid.genMaze(world.getSeed(), 500);
+					System.out.println("Maze generated, at " + maze.getCenter() + ", because world loaded, with seed " + world.getSeed() + ".");
+					break;
+				case 0:
+					break;
+				case 1:
+					break;
+			}
+		}
 	}
+
 	public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
 		return setup(entry, new ResourceLocation(Labrynth.MODID, name));
 	}
---
+++ src/main/java/util/interfaces/IStructure.java
@@ -0,0 +1,11 @@
+package util.interfaces;
+
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.world.gen.feature.template.PlacementSettings;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.gen.feature.structure.Structure;
+
+public class IStructure {
+
+}
